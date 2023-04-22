3a86dc261147ab713965c495cd105dad445dd50a
Mike Renfro <renfro@tntech.edu>
2023-04-22 17:00:51 -0500

    configure r10k webhook on puppet

--- puppet/default.pp
+++ puppet/default.pp
@@ -20,5 +20,60 @@ node 'puppet.theits23.renf.ro' {
   class { 'puppet':
     server         => true,
     server_foreman => false,
+    server_reports => 'store',
+  }
+  package { 'git':
+    ensure => present,
+  }
+  ssh_keygen { 'puppet':
+    home => '/opt/puppetlabs/server/data/puppetserver'
+  }
+  sshkey { 'git.theits23.renf.ro':
+    ensure => present,
+    type   => 'ssh-ed25519',
+    key    => 'AAAAC3NzaC1lZDI1NTE5AAAAICNYDq2jAEFC3yqM9vzDwxrFuqFp8Qz7QgekMR2mBevf'
+    # substitute with output from ssh-keyscan
+  }
+  $webhook_baseurl = 'https://github.com/adnanh/webhook/releases/download/2.8.0'
+  archive { '/usr/local/bin/webhook.tar.gz':
+    source          => "${webhook_baseurl}/webhook-linux-amd64.tar.gz",
+    extract         => true,
+    extract_command => 'tar xfz %s --no-same-owner --strip-components=1',
+    extract_path    => '/usr/local/bin',
+    creates         => '/usr/local/bin/webhook',
+  }
+  file { '/etc/systemd/system/webhook.service':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => '/vagrant/conf/webhook.service',
+  }
+  file { '/etc/webhook.yaml':
+    ensure => present,
+    source => '/vagrant/conf/webhook.yaml',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  service { 'webhook':
+    ensure  => running,
+    enable  => true,
+    require => [
+      File['/etc/systemd/system/webhook.service'],
+      File['/etc/webhook.yaml'],
+      Archive['/usr/local/bin/webhook.tar.gz'],
+    ],
+  }
+  class { 'r10k':
+    remote   => 'git@git.theits23.renf.ro:theits23/puppet-control.git',
+    provider => 'puppet_gem',
+  }
+  file { '/usr/local/bin/r10k-deploy-ref':
+    ensure => present,
+    source => '/vagrant/shell/r10k-deploy-ref',
+    owner  => 'root',
+    group  => 'puppet',
+    mode   => '0750',
   }
 }
